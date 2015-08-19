0c9775712bebb0b91755323990911262ea565c95
Mariemhady <mariem_hady@utlook.com>
2015-08-19 21:41:37 +0200

    Update skills_controller.rb

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -69,6 +69,6 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params.require(:skill).permit(:list_id, :name, :description)
+      params.require(:skill).permit(:user_id, :name, :description)
     end
 end
